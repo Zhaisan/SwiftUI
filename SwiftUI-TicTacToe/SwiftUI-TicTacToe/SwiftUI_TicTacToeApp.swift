4a3cba89c2902151c52ff30db88286b0c8d429a1
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-07-03 23:43:44 +0600

    tic tac toe game with AI

---
+++ SwiftUI-TicTacToe/SwiftUI-TicTacToe/SwiftUI_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TicTacToeApp.swift
+//  SwiftUI-TicTacToe
+//
+//  Created by Zhaisan on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
